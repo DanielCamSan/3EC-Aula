31e77837eaf04b11b04aebc40175724dfadebd37
Joaquinmes18 <joaquineliassoriaa@gmail.com>
2025-10-10 17:45:11 -0400

    fixing carpet DTO

---
+++ Models/DTO/UpdateAppointmentDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Dtos
+{
+    public class UpdateAppointmentDto
+    {
+        public Guid PetId { get; set; }
+
+        [Required]
+        public DateTime ScheduledAt { get; set; }
+
+        [Required, MinLength(3)]
+        public string Reason { get; set; } = string.Empty;
+
+        [Required]
+        public string Status { get; set; } = "scheduled";
+
+        public string? Notes { get; set; }
+    }
+}
