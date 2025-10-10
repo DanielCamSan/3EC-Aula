83f1ba59050008e4ed0a2555cb2c6d1ee1539b7e
unknown <ariana.pita@ucb.edu.bo>
2025-10-10 18:28:43 -0400

    feat: Add IOwner Service

---
+++ Services/IOwnerService.cs
@@ -0,0 +1,15 @@
+﻿using FirstExam.Models;
+using FirstExam.Models.dtos;
+
+namespace FirstExam.Services
+{
+    public interface IOwnerService
+    {
+        Task<Owner> Create(CreateOwnerDto dto);
+        Task<bool> Delete(Guid id);
+        Task<IEnumerable<Owner>> GetAll();
+        Task<Owner?> GetById(Guid id);
+
+        Task<Owner?> Update(Guid id, UpdateOwnerDto dto);
+    }
+}
