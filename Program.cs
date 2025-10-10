d8458b5027df8d01e5433593b165c775d4e927b2
CalebBav234 <calebmbaving@gmail.com>
2025-10-10 17:42:37 -0400

    feat: added dependancy injection for owners

--- Program.cs
+++ Program.cs
@@ -1,22 +1,25 @@
+
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(
-    options =>
-    {
-        options.AddPolicy("MiPoliticaCors", policy =>
-        {
-            policy.WithOrigins("https://localhost:7162", "http://127.0.0.1:5500").AllowAnyMethod().AllowAnyHeader();
-        });
-    });
-
+// Add services to the container.
 
 builder.Services.AddControllers();
 
+
+builder.Services.AddDbContext<FirstExam.Data.AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+//scoped solo para db externo y singleton para db en memoria
+
+builder.Services.AddScoped<FirstExam.Repositories.IOwnerRepository, FirstExam.Repositories.OwnerRepository>();
+builder.Services.AddScoped<FirstExam.Services.IOwnerService, FirstExam.Services.OwnerService>();
+
 var app = builder.Build();
+// Dependency Injection
 
 app.UseHttpsRedirection();
-
-app.UseCors("MiPoliticaCors");
 
 app.UseAuthorization();
 
