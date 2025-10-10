3f380ff87fca79c1165fdbf283f6f6f0b25bf58f
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2025-10-10 17:10:43 -0400

    feat: Appointment DbContext created.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,7 +1,24 @@
-﻿namespace FirstExam.Data
-{
-    public class AppDbContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
-    }
+namespace FirstExam.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Appointment> Appointments => Set<Appointment>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Appointment>(a =>
+            {
+                a.HasKey(x => x.Id);
+                a.Property(x => x.Status).IsRequired().HasMaxLength(200);
+                a.Property(x => x.Notes).IsRequired();
+                a.HasIndex(x => x.PetId);
+                a.Property(x => x.ScheduledAt).IsRequired();
+                a.Property(x => x.Reason).IsRequired();
+            });
+        }
 }
