7c5466b6e9a0bfa5da06898407746127b485480b
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2025-10-10 17:47:13 -0400

    feat: modified controller and add dto,repository, service

---
+++ Repositories/OwnerRepository.cs
@@ -0,0 +1,44 @@
+﻿using FirstExam.Models;
+
+namespace FirstExam.Repositories
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly AppDbContext _context;
+        public OwnerRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(Owner owner)
+        {
+            await _context.Owners.AddAsync(owner);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var owner = await _context.Owners.FirstOrDefaultAsync(x => x.Id == id);
+            if (owner != null)
+            {
+                _context.Owners.Remove(owner);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Owner>> GetAll()
+        {
+            return await _context.Owners.ToListAsync();
+        }
+
+        public async Task<Owner?> GetById(Guid id)
+        {
+            return await _context.Owners.FirstOrDefaultAsync(o  => o.Id == id);
+        }
+
+        public async Task Update(Owner owner)
+        {
+            _context.Owners.Update(owner);
+            await _context._SaveChangesAsync();
+        }
+    }
+}
