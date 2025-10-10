52617f1a38a55dc5e9cf9ad21945fc5e4f8588e8
KathM00 <kathymontamejia@gmail.com>
2025-10-10 18:03:51 -0400

    fix: IOwnerServices

--- Services/IOwnerService.cs
+++ Services/IOwnerService.cs
@@ -5,10 +5,10 @@ namespace FirstExam.Services
 {
     public interface IOwnerService
     {
+        Task<Owner> Create(CreateOwnerDto dto);
+        Task<Owner?> Update(Guid id, UpdateOwnerDto dto);
+        Task<bool> Delete(Guid id);
         Task<IEnumerable<Owner>> GetAll();
         Task<Owner?> GetById(Guid id);
-        Task<Owner> Create(CreateOwnerDto dto);
-        Task<Owner> Update (UpdateOwnerDto dto);
-        Task<bool> Delete(Guid id);
     }
 }
