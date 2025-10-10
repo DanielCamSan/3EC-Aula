690198227eb95dc3d02afd001ab6be646afcfc08
shalure10lure <yessicarado@gmail.com>
2025-10-10 16:54:09 -0400

    feat: adding method Add in OwnerRepository

--- Repositories/OwnerRepository.cs
+++ Repositories/OwnerRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace FirstExam.Repositories
+﻿using FirstExam.Models;
+using System;
+
+namespace FirstExam.Repositories
 {
-    public class OwnerRepository
+    public class OwnerRepository : IOwnerRepository
     {
+        private readonly AppDbContext _context;
+        public OwnerRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Owner owner)
+        {
+            await _context.Owners.AddAsync(owner);
+            await _context.SaveChangesAsync();
+        }
+
     }
 }
