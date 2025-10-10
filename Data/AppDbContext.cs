abcd1ec0dfa973c9d3e775794e4476240ef0543c
Luiscuac <luiguiandre2208@gmail.com>
2025-10-10 17:14:56 -0400

    feat:dbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FirstExam.Models;
+
+namespace FirstExam.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Appointment> Appointments => Set<Appointment>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Appointment>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.PetId).IsRequired();
+                entity.Property(e => e.ScheduledAt).IsRequired();
+                entity.Property(e => e.Reason).HasMaxLength(500);
+                entity.Property(e => e.Status).IsRequired();
+                entity.Property(e => e.Notes).HasMaxLength(1000);
+            });
+        }
+
+    }
+}
