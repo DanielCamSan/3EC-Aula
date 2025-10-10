c98c5c1920708d7d9a1ee6bb9d0870c61380e0ca
KathM00 <kathymontamejia@gmail.com>
2025-10-10 16:53:19 -0400

    feat: UpdateOwnerDto created

---
+++ Models/DTO/UpdateOwnerDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Models.DTO
+{
+    public record UpdateOwnerDto
+    {
+        [Required, StringLength(200)]
+        public string Email { get; set; }
+        [Required, StringLength(200)]
+        public string FullName { get; set; }
+        [Required, StringLength(7, MinimumLength = 7)]
+        public string Phone { get; init; }
+        public bool Active { get; set; }
+    }
+}
