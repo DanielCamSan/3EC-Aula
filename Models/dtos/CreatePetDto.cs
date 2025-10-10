1f3642e1433831fe8937c3cb7245146c4b151c28
Maria <mariajesuslozada10@gmail.com>
2025-10-10 16:53:06 -0400

    feat: createpetDto

---
+++ Models/dtos/CreatePetDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Models.dtos
+{
+    public record CreatePetDto
+    {
+        [Required]
+        public Guid OwnerId { get; set; } = Guid.NewGuid();
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+        [Required, StringLength(100)]
+        public string Species { get; set; } // dog | cat | bird | reptile | other 
+        [Required, StringLength(100)]
+        public string Breed { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+        [Required, StringLength(20)]
+        public string sex { get; set; } // macho | hembra
+        [Range(0, 500)]
+        public decimal? WeightKg { get; set; }
+    }
+}
