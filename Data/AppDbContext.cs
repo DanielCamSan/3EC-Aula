085135160e09de97863d259d9b16719d8747b687
RashLop <lopezmejiarasshid@outlook.com>
2025-10-10 17:09:20 -0400

    feat: Pet's dbContext added

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -12,8 +12,9 @@ namespace FirstExam.Data
 
         }
 
-        public DbSet<Appointment> Appointments => Set<Appointment>(); 
+        public DbSet<Appointment> Appointments => Set<Appointment>();
         public DbSet<Owner> Owners => Set<Owner>();
+        public DbSet<Pet> Pets => Set<Pet>();
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -27,7 +28,7 @@ namespace FirstExam.Data
                 a.Property(x => x.Status).IsRequired().HasMaxLength(100);
                 a.Property(x => x.Notes);
             });
-            
+
             modelBuilder.Entity<Owner>(o =>
             {
                 o.HasKey(x => x.Id);
@@ -36,6 +37,19 @@ namespace FirstExam.Data
                 o.Property(x => x.Phone).IsRequired().HasMaxLength(7);
                 o.Property(x => x.Active);
             });
+            
+            modelBuilder.Entity<Pet>(p =>
+            {
+                p.HasKey(x => x.Id);
+                p.Property(x => x.OwnerId).IsRequired();
+                p.Property(x => x.Name).IsRequired().HasMaxLength(100);
+                p.Property(x => x.Species).IsRequired().HasMaxLength(100);
+                p.Property(x => x.Breed).IsRequired().HasMaxLength(100);
+                p.Property(x => x.BirthDate).IsRequired();
+                p.Property(x => x.sex).IsRequired().HasMaxLength(20);
+                p.Property(x => x.WeightKg);
+            }); 
+
         }
 
     }
