2e5f5b723634142e86f01e7ff311efb3ccdde3d3
Maria <mariajesuslozada10@gmail.com>
2025-10-10 17:23:57 -0400

    feat: PetService and IPetService

---
+++ Services/IPetService.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstExam.Services
+{
+    public interface IPetService
+    {
+
+        Task<IEnumerable<Pet>> GetAll();
+        Task<Pet?> GetByid(int id);
+        T
+
+    }
+}
---
+++ Services/PetService.cs
@@ -0,0 +1,32 @@
+﻿using FirstExam.Models;
+using FirstExam.Models.dtos;
+using FirstExam.Repositories;
+
+namespace FirstExam.Services
+{
+    public class PetService: IPetService
+    {
+        private readonly IPetRepository _repo;
+        public PetService(IPetRepository repo) => _repo = repo;
+
+        public async Task<Pet> Create(CreatePetDto dto)
+        {
+            var pet = new Pet { Id = Guid.NewGuid(),OwnerId = Guid.NewGuid(), Name = dto.Name.Trim(), 
+                Species = dto.Species.Trim(), Breed = dto.Breed , BirthDate = dto.BirthDate, sex = dto.sex, WeightKg = dto.WeightKg };
+            await _repo.Add(pet);
+            return pet;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var existing = await _repo.GetById(id);
+            if (existing is null) return false;
+            await _repo.Delete(id);
+            return true;
+        }
+
+        public Task<IEnumerable<Pet>> GetAll() => _repo.GetAll();
+        public Task<Pet?> GetById(Guid id) => _repo.GetById(id);
+
+    }
+}
