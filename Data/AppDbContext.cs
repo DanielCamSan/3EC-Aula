d44971b29ce91ed0684288d130466f81579ade31
Majo <maria.sandoval.o@ucb.edu.bo>
2025-10-10 17:21:07 -0400

    feat: dbcontext and pet repository

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,27 @@
-﻿namespace FirstExam.Data
+﻿using FirstExam.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+namespace FirstExam.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Pet> Pets => Set<Pet>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pet>(p =>
+            {
+                p.HasKey(x => x.Id);
+                p.Property(x => x.OwnerId).IsRequired();
+                p.Property(x => x.Breed).IsRequired().HasMaxLength(100);
+                p.Property(x => x.sex).IsRequired().HasMaxLength(20);
+                p.Property(x => x.Species).IsRequired().HasMaxLength(100);
+                p.Property(x => x.BirthDate).IsRequired();
+                p.Property(x=> x.WeightKg).IsRequired();
+                p.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            });
+        }
     }
 }
--- Repositories/PetRepository.cs
+++ Repositories/PetRepository.cs
@@ -1,6 +1,41 @@
-﻿namespace FirstExam.Repositories
+﻿
+using FirstExam.Data;
+
+namespace FirstExam.Repositories
 {
-    public class PetRepository
+    public class PetRepository : IPetRepository
     {
+        private readonly AppDbContext _context;
+        public PetRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Add(Pet pet)
+        {
+            await _context.Pets.AddAsync(book);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task Delete(Guid id)
+        {
+            var book = await _context.Pets.FirstOrDefaultAsync(x => x.Id == id);
+            if (book != null)
+            {
+                _context.Pets.Remove(book);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public Task<IEnumerable<Pet>> GetAll()
+        {
+            return await _context.Pets.AsNoTracking()
+        .Include(b => b.Author).ToListAsync();
+        }
+
+        public Task<Pet?> GetById(Guid id)
+        {
+            return await _context.Pets.FirstOrDefaultAsync(x => x.Id == id);
+        }
     }
 }
