1efb44506105e29ea81ef6f99a387553c4a9d12a
unknown <nicole.herbas@ucb.edu.bo>
2025-10-10 16:59:59 -0400

    feat: new I Owner Repository

---
+++ Repositories/IOwnerRepository.cs
@@ -0,0 +1,18 @@
+﻿using FirstExam.Models;
+
+namespace FirstExam.Repositories
+{
+    public interface IOwnerRepository
+    {
+        Task<List<Owner>> GetAllWithBooks();
+        Task<Owner?> GetByIdWithBooks(Guid id);
+        Task<bool> ExistsByName(string name);
+        Task<bool> ExistsByNameExcludingId(string name, Guid excludeId);
+        Task Add(Owner owner);
+        Task Update(Owner owner);
+        Task<bool> Delete(Guid id);   // false si no existe o FK bloquea
+        Task<bool> HasBooks(Guid id); // para proteger borrado
+
+        Task<bool> ExistsById(Guid id);
+    }
+}
