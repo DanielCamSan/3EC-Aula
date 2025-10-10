ae94d0df188f6e913d43c0bd584ea90a4e796dd6
Joaquinmes18 <joaquineliassoriaa@gmail.com>
2025-10-10 17:25:50 -0400

    feat(appointments): add service layer for CRUD, sorting and pagination

---
+++ Services/AppointmentService.cs
@@ -0,0 +1,6 @@
+﻿namespace FirstExam.Services
+{
+    public class AppointmentService
+    {
+    }
+}
---
+++ Services/IAppointmentService.cs
@@ -0,0 +1,6 @@
+﻿namespace FirstExam.Services
+{
+    public class IAppointmentService
+    {
+    }
+}
