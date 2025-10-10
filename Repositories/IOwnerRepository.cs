12890d0559e9b63affef343839e1b62ebe92d110
unknown <ariana.pita@ucb.edu.bo>
2025-10-10 18:25:41 -0400

    feat: Add IOwner Repository

---
+++ Repositories/IOwnerRepository.cs
@@ -0,0 +1,15 @@
+﻿using FirstExam.Models;
+
+namespace FirstExam.Repositories
+{
+    public interface IOwnerRepository 
+    {
+        Task<IEnumerable<Owner>> GetAll();
+        Task<Owner?> GetById(Guid id);
+        Task Add(Owner guest);
+        Task Delete(Guid id);
+
+        //Update
+        Task Update(Owner owner);
+    }
+}
