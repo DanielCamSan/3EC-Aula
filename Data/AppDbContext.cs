b53fde7f3d4b9197a833c58791b950087ac1b2c3
unknown <amira.choque@ucb.edu.bo>
2025-10-10 17:18:54 -0400

    Fix: create AppDbContext in Data's carpet

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,25 @@
-﻿namespace FirstExam.Data
+﻿using apiwithdb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace apiwithdb.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Pet> Books => Set<Pet>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(b =>
+            {
+                b.HasKey(x => x.Id);
+                b.Property(x => x.Title).IsRequired().HasMaxLength(200);
+                b.Property(x => x.Year).IsRequired();
+            });
+        }
     }
 }
