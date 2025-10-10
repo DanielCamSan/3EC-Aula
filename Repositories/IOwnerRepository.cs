99e6ccd0daa84022e0ed546661a352ada75e0c01
shalure10lure <yessicarado@gmail.com>
2025-10-10 16:48:58 -0400

    feat: adding IOwerRepository

---
+++ Repositories/IOwnerRepository.cs
@@ -0,0 +1,12 @@
+﻿using FirstExam.Models;
+
+namespace FirstExam.Repositories
+{
+    public interface IOwnerRepository
+    {
+        Task<IEnumerable<Owner>> GetAll();
+        Task<Owner?> GetById(Guid id);
+        Task Add(Owner owner);
+        Task Delete(Guid id);
+    }
+}
