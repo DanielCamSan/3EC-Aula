6361101e8acb62c67271a8e5a90b063f3a434308
Josue-Camacho <josuecamacho582@gmail.com>
2025-10-10 16:57:24 -0400

    feat: Add Model, create and update files

---
+++ Models/dtos/CreateAppointmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Models.dtos
+{
+   
+        public record CreateAppointmentDto
+        {
+            public Guid PetId { get; init; }
+            public DateTime ScheduledAt { get; init; }
+            [Required, StringLength(100)]
+            public string Reason { get; init; }
+            [Required, StringLength(100)]
+            public string Status { get; init; }
+            public string? Notes { get; init; }
+        };
+}
