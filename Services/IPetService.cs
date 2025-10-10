b43b5406cb0646b330f160f2a34d7fd91d4e30f4
Flora <flora.ortiz@ucb.edu.bo>
2025-10-10 17:05:24 -0400

    feat: adding IPetService functions

--- Services/IPetService.cs
+++ Services/IPetService.cs
@@ -1,7 +1,14 @@
-﻿namespace FirstExam.Services
+﻿using FirstExam.Models;
+using FirstExam.Models.DTO;
+
+namespace FirstExam.Services
 {
     public interface IPetService
     {
-
+        Task<IEnumerable<Pet>> GetAll();
+        Task<Pet?> GetById(Guid id);
+        Task<Pet> Create(CreatePetDto dto);
+        Task<Pet> Update(UpdatePetDto dto);
+        Task<bool> Delete(Guid id);
     }
 }
