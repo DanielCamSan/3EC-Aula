426dde62a9767d9ac1b9a3220dc5c4a40a6bbf65
unknown <nicole.herbas@ucb.edu.bo>
2025-10-10 17:12:25 -0400

    feat: creating appDbContext with owners

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using FirstExam.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FirstExam.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Owner> Owners => Set<Owner>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Owner>(a =>
+            {
+                a.HasKey(x => x.Id);
+                a.Property(x => x.Id).ValueGeneratedNever();
+                a.Property(x => x.Email).IsRequired();
+                a.Property(x => x.FullName).IsRequired().HasMaxLength(200);
+            });
+
+        }
+    }
+}
