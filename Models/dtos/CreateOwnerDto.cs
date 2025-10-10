0b386296bcb5d98d205bba47e928546c7830c3be
Dabner Orozco <dabner.orozco@ucb.edu.bo>
2025-10-10 16:52:56 -0400

    feat: Creating owner.cs and dtos for Owner

---
+++ Models/dtos/CreateOwnerDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Models.dtos
+{
+    public record CreateOwnerDto
+    {
+        public Guid Id { get; set; }
+
+        [Required, StringLength(200)]
+        public string Email { get; set; }
+        [Required, StringLength(200)]
+        public string FullName { get; set; }
+        [Required, StringLength(maximumLength: 20, MinimumLength = 7)]
+        public string Phone { get; set; }
+        public bool Active { get; set; }
+        
+    }
+}
