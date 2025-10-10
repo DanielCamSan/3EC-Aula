c9a1cec5c605ad02693d7866bfb21d5adb7437b9
Dabner Orozco <dabner.orozco@ucb.edu.bo>
2025-10-10 17:13:24 -0400

    feat: Creating IOwnerService for owners

---
+++ Services/IOwnerService.cs
@@ -0,0 +1,14 @@
+﻿using FirstExam.Models;
+using FirstExam.Models.dtos;
+
+namespace FirstExam.Services
+{
+    public interface IOwnerService
+    {
+        Task<IEnumerable<Owner>> GetAll();
+        Task<Owner?> GetById(Guid id);
+        Task Update(Owner owner);
+        Task Add(Owner owner);
+        Task Delete(Guid id);
+    }
+}
