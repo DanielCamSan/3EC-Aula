71c1c3680ea4f175f527e48f7d97531f0de22fe9
Dabner Orozco <dabner.orozco@ucb.edu.bo>
2025-10-10 17:02:23 -0400

    feat: Fixing owner.cs and dtos for Owner

--- Models/dtos/CreateOwnerDto.cs
+++ Models/dtos/CreateOwnerDto.cs
@@ -7,12 +7,11 @@ namespace FirstExam.Models.dtos
         public Guid Id { get; set; }
 
         [Required, StringLength(200)]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
         [Required, StringLength(200)]
-        public string FullName { get; set; }
+        public string FullName { get; set; } = string.Empty;
         [Required, StringLength(maximumLength: 20, MinimumLength = 7)]
-        public string Phone { get; set; }
+        public string Phone { get; set; } = string.Empty;
         public bool Active { get; set; }
-        
     }
 }
--- Models/dtos/UpdateOwnerDto.cs
+++ Models/dtos/UpdateOwnerDto.cs
@@ -1,6 +1,15 @@
-﻿namespace FirstExam.Models.dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Models.dtos
 {
-    public class UpdateOwnerDto
+    public record UpdateOwnerDto
     {
+        [Required, StringLength(200)]
+        public string Email { get; set; } = string.Empty;
+        [Required, StringLength(200)]
+        public string FullName { get; set; } = string.Empty;
+        [Required, StringLength(7, MinimumLength = 7)]
+        public string Phone { get; init; } = string.Empty;
+        public bool Active { get; set; }
     }
 }
