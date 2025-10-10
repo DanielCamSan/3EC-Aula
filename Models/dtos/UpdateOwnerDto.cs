cd2729fb165eff1e4f81af92837a4393d375f7c1
unknown <ariana.pita@ucb.edu.bo>
2025-10-10 16:54:02 -0400

    feat: Update Owner Dto

---
+++ Models/dtos/UpdateOwnerDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Models.dtos
+{
+    public class UpdateOwnerDto
+    {
+        
+        [Required, StringLength(200)]
+        public string Email { get; set; }
+        [Required, StringLength(200)]
+        public string FullName { get; set; }
+        [Required, StringLength(7, MinimumLength = 7)]
+        public string Phone { get; init; }
+        public bool Active { get; set; }
+        
+    }
+}
