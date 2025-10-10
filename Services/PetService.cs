2c181096bc3507b55686eb55ed82448b141979f8
Flora <flora.ortiz@ucb.edu.bo>
2025-10-10 17:15:22 -0400

    feat: adding CreatePet

--- Services/PetService.cs
+++ Services/PetService.cs
@@ -1,7 +1,45 @@
-﻿namespace FirstExam.Services
+﻿using FirstExam.Models.DTO;
+
+namespace FirstExam.Services
 {
-    public class PetService
+    public class PetService : IPetService
     {
 
+        public async Task<Pet> Create(CreatePetDto dto)
+        {
+            var pet = new Pet
+            {
+                Id = Guid.NewGuid(),
+                OwnerId = dto.OwnerId,
+                Name = dto.Name.Trim(),
+                Species = dto.Species.Trim(),
+                Breed = dto.Breed.Trim(),
+                BirthDate = dto.BirthDate,
+                sex = dto.sex,
+                WeightKg = dto.WeightKg,
+            };
+            await _repo.Add(pet);
+            return pet;
+        }
+
+        public Task<bool> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Pet>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Pet?> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Pet> Update(UpdatePetDto dto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
