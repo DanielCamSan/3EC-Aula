544cd94b2a9add235abb5d701f109c3f4447c25b
shalure10lure <yessicarado@gmail.com>
2025-10-10 17:01:56 -0400

    feat: adding methods in IOwnerService

--- Services/IOwnerService.cs
+++ Services/IOwnerService.cs
@@ -1,6 +1,13 @@
-﻿namespace FirstExam.Services
+﻿using FirstExam.Models;
+using FirstExam.Models.dtos;
+
+namespace FirstExam.Services
 {
     public interface IOwnerService
     {
+        Task<IEnumerable<Owner>> GetAll();
+        Task<Owner?> GetById(Guid id);
+        Task<Owner> Create(CreateOwnerDto dto);
+        Task<bool> Delete(Guid id);
     }
 }
