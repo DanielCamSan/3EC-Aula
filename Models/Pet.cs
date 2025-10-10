4e218fd38fff7bc13c4661189d21784d4ea254b6
unknown <amira.choque@ucb.edu.bo>
2025-10-10 17:08:54 -0400

    Fix: update Model of Pet

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -1,56 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
-public class Pet
+namespace apiwithdb.Models
 {
-    [Required]
-    public Guid Id { get; set; }= Guid.NewGuid();
-    [Required]
-    public Guid OwnerId { get; set; } = Guid.NewGuid();
-    [Required, StringLength(100)]
-    public string Name { get; set; }
-    [Required, StringLength(100)]
-    public string Species { get; set; } // dog | cat | bird | reptile | other 
-    [Required, StringLength(100)]
-    public string Breed { get; set; }
-    [Required]
-    public DateTime BirthDate { get; set; }
-    [Required, StringLength(20)]
-    public string sex { get; set; } // macho | hembra
-    [Range(0,500)]
-    public decimal? WeightKg { get; set; }
-}
-
-public record CreatePetDto
-{
-    [Required]
-    public required Guid OwnerId { get; init; }
-    [Required, StringLength(100)]
-    public required string Name { get; init; }
-    [Required, StringLength(100)]
-    public required string Species { get; init; } // dog | cat | bird | reptile | other 
-    [Required, StringLength(100)]
-    public required string Breed { get; init; }
-    [Required]
-    public required DateTime BirthDate { get; init; }
-    [Required, StringLength(20)]
-    public required string sex { get; init; }
-    [Range(0, 500)]
-    public decimal? WeightKg { get; init; }
-}
-
-public record UpdatePetDto
-{
-    [Required]
-    public required Guid OwnerId { get; set; }
-    [Required, StringLength(100)]
-    public required string Name { get; set; }
-    [Required, StringLength(100)]
-    public required string Species { get; set; } // dog | cat | bird | reptile | other 
-    [Required, StringLength(100)]
-    public required string Breed { get; set; }
-    [Required]
-    public required DateTime BirthDate { get; set; }
-    [Required, StringLength(20)]
-    public required string sex { get; set; }
-    [Range(0, 500)]
-    public decimal? WeightKg { get; set; }
+    public class Pet
+    {
+        public Guid Id { get; set; }
+        public Guid OwnerId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Species { get; set; } = string.Empty;// dog | cat | bird | reptile | other 
+        public string Breed { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        public string sex { get; set; } = string.Empty;// macho | hembra
+        public decimal? WeightKg { get; set; }
+    }
 }
