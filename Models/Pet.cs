220d199d8eaaf4a595b6b4fa8526c5caa524f773
Flora <flora.ortiz@ucb.edu.bo>
2025-10-10 16:45:57 -0400

    fix: string.Empty in Pet model

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -6,15 +6,15 @@ public class Pet
     [Required]
     public Guid OwnerId { get; set; } = Guid.NewGuid();
     [Required, StringLength(100)]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     [Required, StringLength(100)]
-    public string Species { get; set; } // dog | cat | bird | reptile | other 
+    public string Species { get; set; } = string.Empty; // dog | cat | bird | reptile | other 
     [Required, StringLength(100)]
-    public string Breed { get; set; }
+    public string Breed { get; set; } = string.Empty ;
     [Required]
     public DateTime BirthDate { get; set; }
     [Required, StringLength(20)]
-    public string sex { get; set; } // macho | hembra
+    public string sex { get; set; } = string.Empty; // macho | hembra
     [Range(0,500)]
     public decimal? WeightKg { get; set; }
 }
