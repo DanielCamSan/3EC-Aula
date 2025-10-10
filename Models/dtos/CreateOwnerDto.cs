c9f92773f166477080790ab9f872771964800b6c
unknown <ariana.pita@ucb.edu.bo>
2025-10-10 16:53:25 -0400

    feat: Create Owner Dto

---
+++ Models/dtos/CreateOwnerDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstExam.Models.dtos
+{
+    public record CreateOwnerDto
+    {
+        public Guid Id { get; init; }
+        [Required, StringLength(200)]
+        public string Email { get; init; }
+        [Required, StringLength(200)]
+        public string FullName { get; init; }
+        [Required, StringLength(maximumLength: 20, MinimumLength = 7)]
+        public string Phone { get; init; }
+        public bool Active { get; init; }
+        
+    }
+}
