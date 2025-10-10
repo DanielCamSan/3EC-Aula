87a71f7d309eb85225827e1386bf47f8bca2ed6a
unknown <amira.choque@ucb.edu.bo>
2025-10-10 17:14:45 -0400

    Fix: update dto of pet

--- Models/dtos/CreatePetDto.cs
+++ Models/dtos/CreatePetDto.cs
@@ -1,6 +1,18 @@
-﻿namespace FirstExam.Models.dtos
+﻿using System.ComponentModel.DataAnnotations;
+namespace apiwithdb.Models.dtos
 {
-    public class CreatePetDto
+    public record CreatePetDto
     {
+        [Required, StringLength(200)]
+        public string Name { get; set; } = string.Empty;
+        [Required, StringLength(200)]
+        public string Species { get; set; } = string.Empty;// dog | cat | bird | reptile | other 
+        [Required, StringLength(200)]
+        public string Breed { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        [Required, StringLength(200)]
+        public string sex { get; set; } = string.Empty;// macho | hembra
+        [Range(1000, 2100)]
+        public decimal? WeightKg { get; set; }
     }
 }
