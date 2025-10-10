7cb46d31547045c301a66f0de6e10626cc6a61c7
KathM00 <kathymontamejia@gmail.com>
2025-10-10 17:39:04 -0400

    fix: IOwnerServices

--- Services/IOwnerService.cs
+++ Services/IOwnerService.cs
@@ -8,6 +8,7 @@ namespace FirstExam.Services
         Task<IEnumerable<Owner>> GetAll();
         Task<Owner?> GetById(Guid id);
         Task<Owner> Create(CreateOwnerDto dto);
+        Task<Owner> Update (UpdateOwnerDto dto);
         Task<bool> Delete(Guid id);
     }
 }
