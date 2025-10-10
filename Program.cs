780303267c61fcb96e941d3cd4c42f98915a8f3a
Dabner Orozco <dabner.orozco@ucb.edu.bo>
2025-10-10 18:09:32 -0400

    feat: creating db for owners

--- Program.cs
+++ Program.cs
@@ -1,8 +1,3 @@
-using FirstExam.Data;
-using FirstExam.Repositories;
-using FirstExam.Services;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(
@@ -10,19 +5,10 @@ builder.Services.AddCors(
     {
         options.AddPolicy("MiPoliticaCors", policy =>
         {
-            policy.WithOrigins("https://localhost:7162", "http://127.0.0.1:5500")
-                  .AllowAnyMethod()
-                  .AllowAnyHeader();
+            policy.WithOrigins("https://localhost:7162", "http://127.0.0.1:5500").AllowAnyMethod().AllowAnyHeader();
         });
     });
 
-// Agregar DbContext con la conexión a PostgreSQL
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Registrar repositorios y servicios
-builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
-builder.Services.AddScoped<IOwnerService, OwnerService>();
 
 builder.Services.AddControllers();
 
@@ -37,4 +23,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
