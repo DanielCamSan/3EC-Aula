583e6f6fff45a49049d80a58ad00e42e77e09256
Majo <maria.sandoval.o@ucb.edu.bo>
2025-10-10 23:27:17 -0400

    fix: appoitnment repository correction

--- Repositories/AppointmentRepository.cs
+++ Repositories/AppointmentRepository.cs
@@ -15,31 +15,31 @@ namespace FirstExam.Repositories
 
         public async Task Add(Appointment book)
         {
-            await _context.appointments.AddAsync(book);
+            await _context.Appointments.AddAsync(book);
         }
 
         public async Task Delete(Guid id)
         {
-            var book = await _context.appointments.FirstOrDefaultAsync(x => x.Id == id);
+            var book = await _context.Appointments.FirstOrDefaultAsync(x => x.Id == id);
             if (book != null)
             {
-                _context.appointments.Remove(book);
+                _context.Appointments.Remove(book);
             }
         }
 
         public async Task<IEnumerable<Appointment>> GetAll()
         {
-            return await _context.appointments.ToListAsync();
+            return await _context.Appointments.ToListAsync();
         }
 
         public async Task<Appointment?> GetById(Guid id)
         {
-            return await _context.appointments.FirstOrDefaultAsync(x => x.Id == id);
+            return await _context.Appointments.FirstOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task Update(Appointment a)
         {
-            _context.appointments.Update(a);
+            _context.Appointments.Update(a);
             await _context.SaveChangesAsync();
         }
     }
