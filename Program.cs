781c2bbe3375a1502da0bceebdf41f5354e1bded
CalebBav234 <calebmbaving@gmail.com>
2025-10-11 11:23:41 -0400

    feat: updated dependency injection to include pets

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddScoped<FirstExam.Repositories.IOwnerRepository, FirstExam.Re
 builder.Services.AddScoped<FirstExam.Services.IOwnerService, FirstExam.Services.OwnerService>();
 builder.Services.AddScoped<FirstExam.Services.IAppointmentServices, FirstExam.Services.AppointmentServices>();
 builder.Services.AddScoped<FirstExam.Repositories.IAppointmentRepository, FirstExam.Repositories.AppointmentRepository>();
+builder.Services.AddScoped<FirstExam.Repositories.IPetRepository, FirstExam.Repositories.PetRepository>();
+builder.Services.AddScoped<FirstExam.Services.IPetService, FirstExam.Services.PetService>();
 var app = builder.Build();
 // Dependency Injection
 
