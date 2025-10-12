3c1c1b0f60522bf62da27a9ec56195065d382d63
Flora <flora.ortiz@ucb.edu.bo>
2025-10-11 23:38:23 -0400

    feat: buildign entities

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,14 +10,40 @@ namespace FirstExam.Data
         {
         }
 
+        public DbSet<Owner> Owners => Set<Owner>();
         public DbSet<Pet> Pets => Set<Pet>();
         public DbSet<Appointment> Appointments => Set<Appointment>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) 
         {
-            modelBuilder.Entity<Appointment>(a => 
+            modelBuilder.Entity<Owner>(o =>
             {
-                a.HasKey(x  => x.Id);
+                o.HasKey(x => x.Id);
+                o.Property(x => x.Email).IsRequired();
+                o.Property(x => x.FullName).IsRequired().HasMaxLength(200);
+                o.Property(x => x.Phone).IsRequired().HasMaxLength(7);
+                o.Property(x => x.Active).HasDefaultValue(true);
+            });
+
+            
+            modelBuilder.Entity<Pet>(p =>
+            {
+                p.HasKey(x => x.Id);
+                p.Property(x => x.Name).IsRequired().HasMaxLength(100);
+                p.Property(x => x.Species).IsRequired().HasMaxLength(100);
+                p.Property(x => x.Breed).IsRequired().HasMaxLength(100);
+                p.Property(x => x.sex).IsRequired().HasMaxLength(20);
+                p.Property(x => x.BirthDate).IsRequired();
+                p.Property(x => x.WeightKg).HasPrecision(0, 500);
+            });
+
+
+            modelBuilder.Entity<Appointment>(a =>
+            {
+                a.HasKey(x => x.Id);
+                a.Property(x => x.Reason).IsRequired().HasMaxLength(100);
+                a.Property(x => x.Status).IsRequired().HasMaxLength(100).HasDefaultValue("scheduled");
+                a.Property(x => x.ScheduledAt).IsRequired();
             });
         }
     }
