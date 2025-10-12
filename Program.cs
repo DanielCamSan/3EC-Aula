e3217b6d382df6237a466440fb401b71c03086a2
Maria <mariajesuslozada10@gmail.com>
2025-10-11 21:58:34 -0400

    feat: inyecting dependencies

--- Program.cs
+++ Program.cs
@@ -2,11 +2,17 @@ using FirstExam.Repositories;
 using FirstExam.Services;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+//inyect dbcontext
 builder.Services.AddDbContext<FirstExam.Data.AppDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
+//inyect dependencias
 builder.Services.AddScoped<FirstExam.Repositories.IAppointmentRepository, FirstExam.Repositories.AppointmentRepository>();
 builder.Services.AddScoped<FirstExam.Services.IAppointmentService, FirstExam.Services.AppointmentService>();
+builder.Services.AddScoped<FirstExam.Repositories.IPetRepository, FirstExam.Repositories.PetRepository>();
+builder.Services.AddScoped<FirstExam.Services.IPetService, FirstExam.Services.PetService>();
+builder.Services.AddScoped<FirstExam.Repositories.IOwnerRepository, FirstExam.Repositories.OwnerRepository>();
+builder.Services.AddScoped<FirstExam.Services.IOwnerService, FirstExam.Services.OwnerService>();
 
 builder.Services.AddCors(
     options =>
