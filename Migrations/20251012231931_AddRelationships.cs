3e90c6556e564abab64f89acbe63cbea0c794171
Maria <mariajesuslozada10@gmail.com>
2025-10-12 19:21:05 -0400

    feat: migrations of relations

---
+++ Migrations/20251012231931_AddRelationships.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirstExam.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pets",
+                table: "Pets");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Appointments",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pets",
+                table: "Pets",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_OwnerId",
+                table: "Pets",
+                column: "OwnerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_OwnerId",
+                table: "Appointments",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_PetId",
+                table: "Appointments",
+                column: "PetId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Owners_OwnerId",
+                table: "Appointments",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Pets_PetId",
+                table: "Appointments",
+                column: "PetId",
+                principalTable: "Pets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_Owners_OwnerId",
+                table: "Pets",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Owners_OwnerId",
+                table: "Appointments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Pets_PetId",
+                table: "Appointments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_Owners_OwnerId",
+                table: "Pets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pets",
+                table: "Pets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pets_OwnerId",
+                table: "Pets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_OwnerId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_PetId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Appointments");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pets",
+                table: "Pets",
+                column: "OwnerId");
+        }
+    }
+}
