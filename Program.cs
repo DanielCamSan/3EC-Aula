4d56a2f867cf62050d4174d9535396981c0cbbd1
Flora <flora.ortiz@ucb.edu.bo>
2025-10-11 23:00:38 -0400

    feat: dependency injection

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 using FirstExam.Repositories;
 using FirstExam.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Controllers
 builder.Services.AddControllers();
-
-// OpenAPI estilo magíster (requiere paquete Microsoft.AspNetCore.OpenApi)
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 // DI: Appointments (repo en memoria = Singleton, service = Scoped)
 builder.Services.AddSingleton<IAppointmentRepository, AppointmentRepository>();
 builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 
+builder.Services.AddScoped<IPetRepository, PetRepository>();
+builder.Services.AddScoped<IPetService, PetService>();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
 app.UseHttpsRedirection();
 
