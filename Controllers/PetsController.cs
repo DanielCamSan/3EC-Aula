4b1eff333233d22de5c8acc84ea0f24e07637ec3
Maria <mariajesuslozada10@gmail.com>
2025-10-11 20:58:26 -0400

    feat: implementing  petsController

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.JSInterop.Infrastructure;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using static System.Runtime.InteropServices.JavaScript.JSType;
-
+using FirstExam.Models.dtos;
+using FirstExam.Services;
+using System.Threading.Tasks;
 
 namespace FirstExam.Controllers
 {
@@ -11,42 +14,12 @@ namespace FirstExam.Controllers
     [Route("api/v1/[controller]")]
     public class PetsController : ControllerBase
     {
-        public static readonly List<Pet> pets = new()
+        private readonly IPetService _petService;
+
+        public PetsController(IPetService petService)
         {
-            new Pet()
-            {
-                Id=Guid.NewGuid(),
-                OwnerId=Guid.NewGuid(),
-                Name="Perla",
-                Species="cat",
-                Breed="egipcio",
-                BirthDate=DateTime.Now.AddMonths(-24),
-                sex="macho",
-                WeightKg=10
-            },
-            new Pet()
-            {
-                Id=Guid.NewGuid(),
-                OwnerId=Guid.NewGuid(),
-                Name="Sadu",
-                Species="dog",
-                Breed="Shar-Pei",
-                BirthDate=DateTime.Now.AddMonths(-60),
-                sex="macho",
-                WeightKg=25
-            },
-            new Pet()
-            {
-                Id=Guid.NewGuid(),
-                OwnerId=Guid.NewGuid(),
-                Name="Fadu",
-                Species="dog",
-                Breed="american bully",
-                BirthDate=DateTime.Now.AddMonths(-36),
-                sex="macho",
-                WeightKg=30
-            }
-        };
+            _petService = petService;
+        } 
 
         public static (int page, int limit) NormalizePage(int? page, int? limit)
         {
@@ -65,79 +38,45 @@ namespace FirstExam.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll([FromQuery] int? Page, [FromQuery] int? Limit, [FromQuery] string? Sort, [FromQuery] string? Order, [FromQuery] string? Q)
+        public async Task<ActionResult<IEnumerable<Pet>> >GetAll()
         {
-            var (p, l) = NormalizePage(Page, Limit);
-            IEnumerable<Pet> query = pets;
-            if (!string.IsNullOrWhiteSpace(Q))
-            {
-                query = query.Where(a => a.Name.Contains(Q, StringComparison.OrdinalIgnoreCase) ||
-                a.Breed.Contains(Q, StringComparison.OrdinalIgnoreCase) ||
-                a.Species.Contains(Q, StringComparison.OrdinalIgnoreCase));
-            }
-            query = OrderByProp(query, Sort, Order);
-            var total = query.Count();
-            var data = query.Skip((p - 1) * l).Take(l).ToArray();
-            return Ok(new { data, meta = new { page = p, limit = l, total } });
-
+            var pets = await _petService.GetAll();
+            return Ok(pets);
         }
 
         [HttpGet("{id:guid}")]
-        public ActionResult<Pet> GetOne(Guid id)
+        public async Task <ActionResult<Pet>> GetOne(Guid id)
         {
-            var pet = pets.FirstOrDefault<Pet>(pets => pets.Id == id);
+            var pet = await _petService.GetById(id);
             return pet is null ? NotFound(new { error = "Pet not found", status = 404 }) : Ok(pet);
         }
 
         [HttpPost]
-        public ActionResult<Pet> Create([FromBody] CreatePetDto dto)
+        public async Task<ActionResult<Pet>> Create([FromBody] CreatePetDto dto)
         {
             if (!ModelState.IsValid) return ValidationProblem(ModelState);
 
-            var pet = new Pet()
-            {
-                Id = Guid.NewGuid(),
-                OwnerId = dto.OwnerId,
-                Name = dto.Name.Trim(),
-                Species = dto.Species.Trim(),               
-                Breed = dto.Breed.Trim(),
-                BirthDate = dto.BirthDate,
-                sex = dto.sex.Trim(),
-                WeightKg = dto.WeightKg
-            };
-            pets.Add(pet);
+            var pet = await _petService.Create(dto);
             return CreatedAtAction(nameof(GetOne), new { id = pet.Id },pet);
         }
 
         [HttpPut("{id:guid}")]
-        public ActionResult<Pet> Update(Guid id, [FromBody] UpdatePetDto dto)
+        public async Task<ActionResult<Pet>> Update(Guid id, [FromBody] UpdatePetDto dto)
         {
             if (!ModelState.IsValid) return ValidationProblem(ModelState);
-            var index = pets.FindIndex(a => a.Id == id);
-            if (index == -1)
+            var updated = await _petService.Update(id, dto);
+            if (updated == null)
                 return NotFound(new { error = "Pet not found", status = 404 });
-            var updated = new Pet()
-            {
-                Id = id,
-                OwnerId = dto.OwnerId,
-                Name = dto.Name.Trim(),
-                Species = dto.Species.Trim(),
-                Breed = dto.Breed.Trim(),
-                BirthDate = dto.BirthDate,
-                sex = dto.sex.Trim(),
-                WeightKg = dto.WeightKg
-            };
-            pets[index] = updated;
+            
             return Ok(updated);
-
         }
+
         [HttpDelete("{id:guid}")]
-        public IActionResult Delete(Guid id)
+        public async Task<IActionResult> Delete(Guid id)
         {
-            var removed = pets.RemoveAll(pet => pet.Id == id);
-            return removed == 0 ?
-                NotFound(new { error = "Pet not found", status = 404 }) :
-            NoContent();
+            var removed = await _petService.Delete(id);
+            return removed ? NoContent() : NotFound(new { error = "Pet not found", status = 404 });
+            
         }
     }
 }
