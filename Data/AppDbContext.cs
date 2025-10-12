0716a6589585f9e7465b8ed14db40b78d4ded61c
Flora <flora.ortiz@ucb.edu.bo>
2025-10-11 23:12:48 -0400

    feat: building entities

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,10 +1,24 @@
-﻿using FirstExam.Models;
+﻿using System.Collections.Generic;
+using FirstExam.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FirstExam.Data
 {
-    public class AppDbContext
+    public class AppDbContext: DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
 
+        public DbSet<Pet> Pets => Set<Pet>();
+        public DbSet<Appointment> Appointments => Set<Appointment>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Appointment>(a => 
+            {
+                a.HasKey(x  => x.Id);
+            });
+        }
     }
 }
