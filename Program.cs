9b157165b3fb9f0fc567139a62df54bfaaafdc2c
Flora <flora.ortiz@ucb.edu.bo>
2025-10-12 21:19:40 -0400

    fix: dependency injections

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
+using FirstExam.Data;
 using FirstExam.Repositories;
+using FirstExam.Repository;
 using FirstExam.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Controllers
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // DI: Appointments (repo en memoria = Singleton, service = Scoped)
 builder.Services.AddSingleton<IAppointmentRepository, AppointmentRepository>();
@@ -15,6 +19,9 @@ builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 
 builder.Services.AddScoped<IPetRepository, PetRepository>();
 builder.Services.AddScoped<IPetService, PetService>();
+
+builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
+builder.Services.AddScoped<IOwnerService, OwnerService>();
 
 var app = builder.Build();
 
