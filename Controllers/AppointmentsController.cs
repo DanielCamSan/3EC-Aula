e45298293de8cdcc6fa0212b6364302840193e37
Dabner Orozco <dabner.orozco@ucb.edu.bo>
2025-10-13 02:50:10 -0400

    fix: Appointments controller route

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace FirstExam.Controllers
 {
     [ApiController]
-    [Route("api/v1/[controller]")]
+    [Route("api/[controller]")]
     public class AppointmentsController : ControllerBase
     {
         private readonly IAppointmentService _service;
