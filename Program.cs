8863a436f04650f290849e0bef1b52f9abb523dc
Flora <flora.ortiz@ucb.edu.bo>
2025-10-13 10:02:23 -0400

    fix: program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
