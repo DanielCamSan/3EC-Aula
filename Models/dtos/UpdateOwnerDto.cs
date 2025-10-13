a2f85ed4af7eed2df90a1d544c46490b7b34cfb4
unknown <ariana.pita@ucb.edu.bo>
2025-10-13 01:21:24 -0400

    fix: Update Owner model and DTOs

--- Models/dtos/UpdateOwnerDto.cs
+++ Models/dtos/UpdateOwnerDto.cs
@@ -4,14 +4,14 @@ using System.ComponentModel.DataAnnotations;
 namespace FirstExam.Models.dtos
 {
     public class UpdateOwnerDto
-    {
-        
+    {        
         [Required, StringLength(200)]
         public string Email { get; set; }
         [Required, StringLength(200)]
         public string FullName { get; set; }
         [Required, StringLength(7, MinimumLength = 7)]
         public string Phone { get; init; }
+        [Required]
         public bool Active { get; set; }
         
     }
