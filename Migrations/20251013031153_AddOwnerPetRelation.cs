9391f7eea60786b3dd5cb44ade934e25d31b490a
KathM00 <kathymontamejia@gmail.com>
2025-10-12 23:14:59 -0400

    feat: Migrations created

---
+++ Migrations/20251013031153_AddOwnerPetRelation.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirstExam.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOwnerPetRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_OwnerId",
+                table: "Pets",
+                column: "OwnerId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_Owners_OwnerId",
+                table: "Pets",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_Owners_OwnerId",
+                table: "Pets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pets_OwnerId",
+                table: "Pets");
+        }
+    }
+}
