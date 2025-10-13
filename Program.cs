d4f3de71f5bc03213ad8ac0eb9610c8a021e12e1
unknown <ariana.pita@ucb.edu.bo>
2025-10-13 01:13:10 -0400

    fix: Update DI registration for Owner

--- Program.cs
+++ Program.cs
@@ -1,22 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddCors(
-    options =>
-    {
-        options.AddPolicy("MiPoliticaCors", policy =>
-        {
-            policy.WithOrigins("https://localhost:7162", "http://127.0.0.1:5500").AllowAnyMethod().AllowAnyHeader();
-        });
-    });
-
 
 builder.Services.AddControllers();
 
+//
+builder.Services.AddDbContext<FirstExam.Data.AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+
+//scoped solo para db externo y singleton para db en memoria
+
+builder.Services.AddScoped<FirstExam.Repositories.IOwnerRepository, FirstExam.Repositories.OwnerRepository>();
+builder.Services.AddScoped<FirstExam.Services.IOwnerService, FirstExam.Services.OwnerService>();
+
+//
+
 var app = builder.Build();
 
-app.UseHttpsRedirection();
 
-app.UseCors("MiPoliticaCors");
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
