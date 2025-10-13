2a516cd5c2ec6d713eaeb501f98e13ce8a50b51a
Flora <flora.ortiz@ucb.edu.bo>
2025-10-13 13:37:13 -0400

    create Migration

--- Migrations/20251013173331_AddOwnerToAppointment.Designer.cs
+++ Migrations/20251013173331_AddOwnerToAppointment.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FirstExam.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FirstExam.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251013173331_AddOwnerToAppointment")]
+    partial class AddOwnerToAppointment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
